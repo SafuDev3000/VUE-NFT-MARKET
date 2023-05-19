396a3c887affd24e861d8d53c0ad11aabb4d50b4
SafuDev3000 <101217823+SafuDev3000@users.noreply.github.com>
2023-05-19 14:44:44 -0700

    implememnt NFTList

--- src/components/NFTList.vue
+++ src/components/NFTList.vue
@@ -12,3 +12,50 @@
 <script>
 import { WalletSDK } from '@coinbase/wallet-sdk';
 import { SafeAppsSdk } from '@gnosis.pm/safe-apps-sdk';
+
+
+export default {
+  data() {
+    return {
+      nfts: [],
+    };
+  },
+  mounted() {
+    this.fetchNFTs();
+  },
+  method: {
+    async fetchNFTs() {
+      // Initialize the wallet SDK 
+      const wallet = new WalletSDK();
+      
+      //connect to the user's wallet 
+      const walletConnection = await wallet.connect();
+
+      //Get the user's address 
+      const address = await walletConnection.getAccount();
+
+      // Use the address to feth the user's NFTs
+      const nfts = await fetchNFTsFromWallet(address);
+
+      //Update the component data with the fetched NFTs
+      this.nfts = nfts;
+    
+
+    },
+    async fetchNFTsFromWallet(address) {
+      // Use the required dependencies to fetch NFTs from the user's wallet 
+      // You'll need to write the logic specific to the wallet SDK or provider you're using
+      // Here's an example using the WalletConnect provider and Web3
+
+    const privider = new Web3Modal().connectTo('walletconnect');
+    const web3 = new Web3(provider);
+
+    // use the web3 isntance to fetch the user's NFTs using the Wallet's API or contract interactions
+
+    // Return the fetched nFTs
+    return fetchedNFTs;
+
+    },
+  },
+};
+<script>
